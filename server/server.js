0bc3856e69205c14505cef8cb60c76f6bb48b12c
abequinonez <abequinonez@users.noreply.github.com>
2018-04-26 13:07:10 -0700

    feat: Add and configure Socket.io

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
-const express = require('express');
 const path = require('path');
+const http = require('http');
+
+const express = require('express');
+const socketIO = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 // Use port 3000 if no environment variable is set
 const port = process.env.PORT || 3000;
@@ -10,6 +15,15 @@ const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+// Listen for a new connection
+io.on('connection', socket => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
 });
