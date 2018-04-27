4323baddbf0490db99aa789752c0548fa61c7fc9
abequinonez <abequinonez@users.noreply.github.com>
2018-04-26 18:16:14 -0700

    feat: Add message events

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,17 @@ const socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: 'Abraham',
+    text: 'Hey!'
+  });
 });
 
 socket.on('disconnect', function() {
   console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message) {
+  console.log('newMessage', message);
 });
--- server/server.js
+++ server/server.js
@@ -19,6 +19,16 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Mark',
+    text: 'Hello!',
+    createdAt: Date.now()
+  });
+
+  socket.on('createMessage', message => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
