71c556c1b09ff45f2c4210c9ec25931f2ff47644
abequinonez <abequinonez@users.noreply.github.com>
2018-04-27 11:18:17 -0700

    refactor: Move message creation into separate function

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const { generateMessage } = require('./utils/message');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
@@ -20,25 +22,19 @@ io.on('connection', socket => {
   console.log('New user connected');
 
   // Greet a new user
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chat app!',
-    createdAt: new Date().getTime()
-  });
+  socket.emit(
+    'newMessage',
+    generateMessage('Admin', 'Welcome to the chat app!')
+  );
 
   // Let others know that a new user has joined
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'A new user has joined',
-    createdAt: new Date().getTime()
-  });
+  socket.broadcast.emit(
+    'newMessage',
+    generateMessage('Admin', 'A new user has joined')
+  );
 
   socket.on('createMessage', message => {
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
   });
 
   socket.on('disconnect', () => {
